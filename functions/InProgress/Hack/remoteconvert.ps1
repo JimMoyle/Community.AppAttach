c04479704d2b6e0e251dd086c467fd3235758a9a
JimMoyle <github@jimmoyle.com>
2025-09-16 15:48:39 +0100

    now a function for remoteconvert

--- functions/InProgress/Hack/remoteconvert.ps1
+++ functions/InProgress/Hack/remoteconvert.ps1
@@ -1,63 +1,95 @@
-#requires -modules 'powershell-yaml', 'microsoft.winget.client'
+function ConvertTo-Msix {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]$TemplatePath,
 
-. .\functions\InProgress\Hack\Update-CaaMptTemplate.ps1
-. .\functions\InProgress\Hack\New-CaaMsixName.ps1
-. .\functions\InProgress\Hack\Format-CaaVersion.ps1
+        [Parameter(Mandatory)]
+        [string]$WingetId,
 
-#Change these parameters
+        [Parameter(Mandatory)]
+        [string]$ExportRootPath,
 
-$TemplatePath = "C:\GitHub\Community.AppAttach-1\functions\InProgress\Hack\RemoteConvertTemplate.xml"
-$WingetId = 'Microsoft.VisualStudioCode'
-$ExportRootPath = "C:\JimM\WingetDownloads"
-$CertHash = '3ap7qhtey6z62'
-$ConverterMachineName = 'Target-0'
-$UserName = 'jimadmin@jimoyle.com'
-$machinePass = get-content "C:\JimM\pass.txt"
-$CertPublisherName = "CN=JimMoyleMsixCert"
+        [Parameter(Mandatory)]
+        [string]$CertHash,
 
-#Stop changing
+        [Parameter(Mandatory)]
+        [string]$ConverterMachineName,
 
-#script
+        [Parameter(Mandatory)]
+        [string]$UserName,
 
-$downloadFolder = Join-Path -Path $ExportRootPath -ChildPath $WingetId
-$TemplateSaveLocation = Join-Path -Path $downloadFolder -ChildPath ("$WingetId" + ".xml")
+        [Parameter(Mandatory)]
+        [string]$MachinePass,
 
-New-Item -Path $downloadFolder -ItemType Directory -Force | Out-Null
+        [Parameter(Mandatory)]
+        [string]$CertPublisherName
+    )
+    begin {
+        ]
+        #requires -modules 'powershel-yaml', 'Microsoft.WinGet.Client'
+        # Import required scripts
+        . .\functions\InProgress\Hack\Update-CaaMptTemplate.ps1
+        . .\functions\InProgress\Hack\New-CaaMsixName.ps1
+        . .\functions\InProgress\Hack\Format-CaaVersion.ps1
+    }
+    process {
 
-$download = Export-WinGetPackage -Id $WingetId -DownloadDirectory $downloadFolder -MatchOption Equals
 
-if ($download.status -ne 'OK') {
-    Write-Error "Failed to download package"
-    return
+        $downloadFolder = Join-Path -Path $ExportRootPath -ChildPath $WingetId
+        $TemplateSaveLocation = Join-Path -Path $downloadFolder -ChildPath ("$WingetId" + ".xml")
+
+        New-Item -Path $downloadFolder -ItemType Directory -Force | Out-Null
+
+        $download = Export-WinGetPackage -Id $WingetId -DownloadDirectory $downloadFolder -MatchOption Equals
+
+        if ($download.status -ne 'OK') {
+            Write-Error "Failed to download package"
+            return
+        }
+
+        $yamlfile = Get-ChildItem -Path $downloadFolder | Where-Object { $_.Extension -eq '.yaml' }
+        $installfile = Get-ChildItem -Path $downloadFolder | Where-Object { $_.Extension -ne '.yaml' -and $_.Extension -ne '.xml' }
+
+        $yaml = Get-Content -Raw -Path $yamlfile.FullName
+        $yamlObject = ConvertFrom-Yaml -Yaml $yaml
+
+        $version = $yamlObject.PackageVersion
+        $formattedVersion = (Format-CaaVersion -Version $version).Version
+        $fileName = New-CaaMsixName -PackageIdentifier $yamlObject.PackageIdentifier -CertHash $CertHash -Version $formattedVersion -Architecture $yamlObject.Installers.Architecture
+
+        $params = @{
+            Path                 = $TemplatePath
+            PackageName          = $yamlObject.PackageIdentifier
+            PackageDisplayName   = $yamlObject.PackageName
+            InstallerPath        = $installfile.FullName.ToString()
+            ComputerName         = $ConverterMachineName
+            UserName             = $UserName
+            Version              = $formattedVersion
+            TemplateSaveLocation = $TemplateSaveLocation
+            InstallerSwitches    = $yamlObject.Installers.InstallerSwitches.Silent + ' ' + $yamlObject.Installers.InstallerSwitches.Custom
+            PublisherName        = $CertPublisherName
+            PublisherDisplayName = $yamlObject.Publisher
+            ShortDescription     = $yamlObject.ShortDescription
+            PackageSaveLocation  = Join-Path -Path $downloadFolder -ChildPath $fileName.Name
+        }
+        Update-CaaMptTemplate @params
+
+        $outputPackage = Start-Process MSIXPackagingTool.exe -ArgumentList "create-package --template $TemplateSaveLocation --machinePassword $MachinePass" -Wait -Passthru -NoNewWindow
+        Write-Output $outputPackage
+    }
+    end {}
 }
 
-$yamlfile = Get-ChildItem -Path $downloadFolder  | where-object {$_.Extension -eq '.yaml'}
-$installfile = Get-ChildItem -Path $downloadFolder  | where-object {$_.Extension -ne '.yaml' -and $_.Extension -ne '.xml'}
-
-$yaml = Get-Content -Raw -Path $yamlfile.FullName
-
-$yamlObject = ConvertFrom-Yaml -Yaml $yaml
-
-$version = $yamlObject.PackageVersion
-$formattedVersion = (Format-CaaVersion -Version $version).Version
-$fileName = New-CaaMsixName -PackageIdentifier $yamlObject.PackageIdentifier -CertHash $CertHash -Version $formattedVersion -Architecture $yamlObject.Installers.Architecture
 $params = @{
-    Path = $TemplatePath
-    PackageName = $yamlObject.PackageIdentifier
-    PackageDisplayName = $yamlObject.PackageName
-    InstallerPath = $Installfile.FullName.ToString()
-    ComputerName = $ConverterMachineName
-    UserName = $UserName
-    Version = $formattedVersion
-    TemplateSaveLocation = $TemplateSaveLocation
-    InstallerSwitches = $yamlObject.Installers.InstallerSwitches.Silent + ' ' + $yamlObject.Installers.InstallerSwitches.Custom
-    PublisherName = $CertPublisherName
-    PublisherDisplayName = $yamlObject.Publisher
-    ShortDescription = $yamlObject.ShortDescription
-    PackageSaveLocation = Join-Path -Path $downloadFolder -ChildPath $fileName.Name
+    TemplatePath         = 'D:\GitHub\Community.AppAttach\functions\InProgress\Hack\RemoteConvertTemplate.xml'
+    WingetId             = 'Microsoft.Azure.StorageExplorer'
+    ExportRootPath       = 'C:\JimM\WingetDownloads'
+    CertHash             = '3ap7qhtey6z62'
+    ConverterMachineName = 'Target-0'
+    UserName             = 'JimAdmin@jimmoyle.com'
+    MachinePass          = Get-Content c:\jimmm\pass.txt
+    CertPublisherName    = 'Microsoft'
 }
-Update-CaaMptTemplate @params
 
-
-
-$outputPackage = Start-Process MSIXPackagingTool.exe -ArgumentList "create-package --template $TemplateSaveLocation --machinePassword $machinePass" -Wait -Passthru -NoNewWindow
+ConvertTo-Msix @params
